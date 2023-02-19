121dc12c7afd6cb21e6868a5e8651253cb6fb8b3
jashgudhka <54763085+jashgudhka@users.noreply.github.com>
2023-02-19 20:37:16 +0530

    This is a number guessing game, a mini project. We will use the random module included in Python to generate a random number and then the user will try to guess the number. We will also add the functionality to limit the number of tries a user can guess the number, just to make it more fun and interactive.

---
+++ Number-Guesser.py
@@ -0,0 +1,3 @@
+import random
+r =random.randrange(0,11) #Randrange is a function which will generate a number within the range of '0' to '10'. Notice that I have put 11 in the upper bound range but it will only one generate the number lower from the given range. In this case the upper bound is 11 and the maximum number would be generated is 10.
+print(r)
