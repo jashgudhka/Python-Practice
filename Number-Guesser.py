ac92ad87a3806287ea9c7f6df9f44117783a1ef0
jashgudhka <54763085+jashgudhka@users.noreply.github.com>
2023-02-19 21:31:22 +0530

    Added the functionality to take the input form the user for the Upper bound of the range from which the number will be generated. It will generate a number from '0' to the number given by user.
    The input given by the user is converted to an integer data type because by default python converts the input into a string data type.
    
    The input is then cheked whether the number given by user is a valid number or not using the if else condition, If the given number is 0 or less than zero, it prompt the user to enter a valid number next time or enter a number greater than 0and  will kill the program.

--- Number-Guesser.py
+++ Number-Guesser.py
@@ -1,3 +1,20 @@
 import random
-r =random.randrange(0,11) #Randrange is a function which will generate a number within the range of '0' to '10'. Notice that I have put 11 in the upper bound range but it will only one generate the number lower from the given range. In this case the upper bound is 11 and the maximum number would be generated is 10.
-print(r)
+
+#Taking input from user to enter the top of the range from which the number will be generated. The lower bound of the range will be set to '0'.
+top_of_Range = input("Type a number: ")
+
+#Checking to see if the input given by user is a number or a string.
+if top_of_Range.isdigit():
+    top_of_Range = int(top_of_Range) #Coverting the input into 'int', because python by default converts the inputs to string. This will also convert the float number given by user to the nearest integer, which eliminates the possibilty of having the random number range in float or any other number type.
+    if top_of_Range <=0:
+        print("Please enter a number greater than zero next time.")
+        quit()
+else:
+    print("Please enter a valid number.")
+    quit()
+
+#Randrange is a function which will generate a number within the range of '0' to '10'. Notice that I have put 11 in the upper bound range but it will only one generate the number lower from the given range. In this case the upper bound is 11 and the maximum number would be generated is 10.
+#To include the given parameter as the upper bound, you can use the 'randint()' function. It will generate a random integer which includes both given parameters as the upper and lower bounds, respectively.
+#If you only give one parameter to the function, then '0' will be considered as the lower bound and the given number will be considered as the upper bound. But also note that this approach will not work with negative numbers. For that you will have to add both parameters.
+random_number =random.randrange(top_of_Range)
+print(random_number)
